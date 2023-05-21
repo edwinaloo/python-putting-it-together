e77973939885dca3b3165bef0f1f381cdb627bea
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-21 15:29:33 +0300

    4tests passing

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+# Test the Book class
+try:
+    book = Book("Harry Potter", 500)
+except TypeError as e:
+    print(str(e))
+
+# expected_message = "page_count must be an integer"
+# assert error_message == expected_message, f"Error message mismatch: {error_message} != {expected_message}
+
+book = Book("Harry Potter", 500)
+book.turn_page()
+pass
     
         
